f54870ef0b6a5d61709cfc2bf4689973fcee99d1
julien-muke <julien.muke@gmail.com>
2023-07-02 10:24:27 +0200

    google auth provider

--- lib/session.ts
+++ lib/session.ts
@@ -2,32 +2,43 @@ import { getServerSession } from "next-auth/next";
 import { NextAuthOptions, User } from "next-auth";
 import { AdapterUser } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
-import jsonwebtoken from 'jsonwebtoken'
+import jsonwebtoken from "jsonwebtoken";
 import { JWT } from "next-auth/jwt";
 
 export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
-            clientId: '',
-            clientSecret: ''
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         })
     ],
-    jwt: {
-        encode: ({ secret, token }) => {
+    // jwt: {
+    //     encode: ({ secret, token }) => {
 
-        },
-        decode: ({ secret, token }) => {
+    //     },
+    //     decode: ({ secret, token }) => {
             
-        }
-    },
+    //     }
+    // },
     theme: {
         colorScheme: 'light',
         logo: '/logo.png'
     },
     callbacks: {
         async session({ session }) {
-
+            return session;
         },
-        async signIn({ user })
+        async signIn({ user }: { user: AdapterUser | User }) {
+            try {
+                // get the user if they exist
+
+                // if they don't exixt, create them
+
+              return true
+            } catch (error: any) {
+                console.log(error);
+                return false;
+            }
+        }
     }
 }
