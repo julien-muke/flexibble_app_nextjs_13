c4d6a473422608909fa17463281571bb1db27c7b
julien-muke <julien.muke@gmail.com>
2023-07-09 16:09:00 +0200

    categories component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { ProjectInterface } from "@/common.types";
+import Categories from "@/components/Categories";
 import ProjectCard from "@/components/ProjectCard";
 import { fetchAllProjects } from "@/lib/actions";
 
@@ -14,15 +15,23 @@ type ProjectSearch = {
     }
 }
 
-const Home = async () => {
-    const data = await fetchAllProjects() as ProjectSearch;
+type SearchParams = {
+    category?: string;
+}
+
+type Props = {
+    searchParams: SearchParams
+}
+
+const Home = async ({ searchParams: { category }}: Props) => {
+    const data = await fetchAllProjects(category) as ProjectSearch;
 
     const projectsToDisplay = data?.projectSearch?.edges || [];
 
     if(projectsToDisplay.length === 0) {
         return (
             <section className="flexStart flex-col paddings">
-                Categories
+                <Categories />
 
                 <p className="no-result-text text-center">No projects found, go create some first.</p>
             </section>
@@ -30,8 +39,8 @@ const Home = async () => {
     }
 
     return (
-        <section className="flex-start flex-col padding mb-16">
-            <h1>Categories</h1>
+        <section className="flexStart flex-col paddings mb-16">
+            <Categories/>
 
             <section className="projects-grid">
                 {projectsToDisplay.map(({ node }: { node: 
